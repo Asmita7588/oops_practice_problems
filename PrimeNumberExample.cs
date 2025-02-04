12f62b4c80f42ed8bb6b33b1367c1b3be8ac4e3b
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-04 17:07:52 +0530

    find prime numbners

---
+++ PrimeNumberExample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProgramOops
+{
+    internal class PrimeNumberExample
+    {
+        public bool IsPrime(int number)
+        {
+            if (number <= 1) return false;
+            for (int i = 2; i <= Math.Sqrt(number); i++)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using PracticeProgramOops;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        PrimeNumberExample primeNumberExample = new PrimeNumberExample();
+        Console.Write("Enter a number: ");
+        int n = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Prime numbers up to {n} are:");
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (primeNumberExample.IsPrime(i))
+            {
+                Console.Write(i + " ");
+            }
+        }
+
+
+        PlaceToVisit place = new PlaceToVisit();
+         place.PlaceWantToVisit();
+    }
+}
