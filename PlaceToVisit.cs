03c74be2ecaabe119134eb3fc4e57f91b7c27632
Asmita Girhepunje <girhepunjeasmita25@gmail.com>
2025-02-04 17:13:08 +0530

    represent_palce_in_upperCase_and_lowercase_where_you_want_to visit

---
+++ PlaceToVisit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeProgramOops
+{
+    internal class PlaceToVisit
+    {
+        public void PlaceWantToVisit()
+        {
+            Console.WriteLine("Enter a place which you want to visit :");
+            string placeName = Console.ReadLine();
+
+            Console.WriteLine("UpperCase " + placeName.ToUpper());
+            Console.WriteLine("LowerCase " + placeName.ToLower());
+        }
+    }
+}
